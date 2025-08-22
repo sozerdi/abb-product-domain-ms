9b183f8ef80a8ca6baff0629029c82e10c15f482
Shamistanh <004461@ibar.int>
2025-08-22 16:15:59 +0400

    DCT-36251 : duplicate unit test

--- src/main/java/az/abb/customer/dto/info/ContactDetailsDto.java
+++ src/main/java/az/abb/customer/dto/info/ContactDetailsDto.java
@@ -5,21 +5,8 @@ import lombok.Data;
 
 @Builder
 @Data
-public class ContactDetailsDto {
+public class ContactDetailsDto extends ProfessionalContactDetailsDto{
 
-    private String email;
-    private String mobile1;
-    private String mobile2;
-    private String mobile3;
-    private String homeTelephone;
-    private String workTelephone;
-    private String workExtensionPhoneNumber;
-    private String socialNetworkMember;
-    private String socialNetworkId;
-    private String alternativeEmail;
-    private String relativeTelephone;
-    private String friendTelephone;
-    private String colleagueTelephone;
     private String telephoneIsd;
     private String mobileIsd;
 }
